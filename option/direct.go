a42d2e6fd16d9bd359c828e9d527430e05d1ed4b
Eken Chan <maskedeken@yahoo.com>
2023-07-06 10:58:53 +0800

    feature: TLS client hello fragment

--- option/direct.go
+++ option/direct.go
@@ -9,7 +9,13 @@ type DirectInboundOptions struct {
 
 type DirectOutboundOptions struct {
 	DialerOptions
-	OverrideAddress string `json:"override_address,omitempty"`
-	OverridePort    uint16 `json:"override_port,omitempty"`
-	ProxyProtocol   uint8  `json:"proxy_protocol,omitempty"`
+	OverrideAddress string    `json:"override_address,omitempty"`
+	OverridePort    uint16    `json:"override_port,omitempty"`
+	ProxyProtocol   uint8     `json:"proxy_protocol,omitempty"`
+	Fragment        *Fragment `json:"fragment,omitempty"`
+}
+
+type Fragment struct {
+	Length   string `json:"length"`
+	Interval string `json:"interval"`
 }
