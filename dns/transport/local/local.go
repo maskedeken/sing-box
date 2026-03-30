46be333c8634a2dbf5daec90030f700132e82101
Eken Chan <maskedeken@yahoo.com>
2026-03-30 10:49:40 +0800

    fix android local dns

--- dns/transport/local/local.go
+++ dns/transport/local/local.go
@@ -4,6 +4,9 @@ package local
 
 import (
 	"context"
+	"net"
+	"net/netip"
+	"runtime"
 
 	"github.com/sagernet/sing-box/adapter"
 	C "github.com/sagernet/sing-box/constant"
@@ -79,6 +82,36 @@ func (t *Transport) Close() error {
 func (t *Transport) Reset() {
 }
 
+func (t *Transport) lookupIP(ctx context.Context, msgId uint16, question mDNS.Question) (*mDNS.Msg, error) {
+	domain := dns.FqdnToDomain(question.Name)
+
+	ips, err := net.LookupIP(domain)
+	if err != nil {
+		if dnsErr, ok := err.(*net.DNSError); ok && dnsErr.IsNotFound {
+			return nil, dns.RcodeError(3) // NXDOMAIN
+		}
+		return nil, err
+	}
+
+	var addresses []netip.Addr
+	for _, ip := range ips {
+		parsed, err := netip.ParseAddr(ip.String())
+		if err != nil {
+			continue
+		}
+		if (question.Qtype == mDNS.TypeA && parsed.Is4()) ||
+			(question.Qtype == mDNS.TypeAAAA && parsed.Is6()) {
+			addresses = append(addresses, parsed)
+		}
+	}
+
+	if len(addresses) == 0 {
+		return nil, dns.RcodeError(0) // NOERROR but empty answer
+	}
+
+	return dns.FixedResponse(msgId, question, addresses, C.DefaultDNSTTL), nil
+}
+
 func (t *Transport) Exchange(ctx context.Context, message *mDNS.Msg) (*mDNS.Msg, error) {
 	if t.resolved != nil {
 		return t.resolved.Exchange(ctx, message)
@@ -88,6 +121,10 @@ func (t *Transport) Exchange(ctx context.Context, message *mDNS.Msg) (*mDNS.Msg,
 		addresses := t.hosts.Lookup(dns.FqdnToDomain(question.Name))
 		if len(addresses) > 0 {
 			return dns.FixedResponse(message.Id, question, addresses, C.DefaultDNSTTL), nil
+		}
+		// Use net.LookupIP on Android or when preferGo is set
+		if runtime.GOOS == "android" || t.preferGo {
+			return t.lookupIP(ctx, message.Id, question)
 		}
 	}
 	return t.exchange(ctx, message, question.Name)
